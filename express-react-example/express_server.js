c0167408ca206d81ea17d91f4b13c8b6038a4462
Anibal Fuentes <anibal.fuentes@impresee.com>
2020-11-25 16:37:54 -0300

    change server port

--- express-react-example/express_server.js
+++ express-react-example/express_server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const port = 3000
+const port = 5010
 
 //Use statics from react
 app.use(express.static(path.join(__dirname, 'build')));
